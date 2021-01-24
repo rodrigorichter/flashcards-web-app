fe1e7ed318b51508a40af40837720037e07623b7
Rodrigo Richter <guigorichter@gmail.com>
2021-01-24 00:05:02 -0300

    added auth0 authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,14 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Application from '../Application/Application';
-import Login from '../Login/Login';
+import Application from './Application';
+import LoginButton from './LoginButton';
 
 function App() {
-  const [token, setToken] = useState();
-
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
 
   return (
     <div className="wrapper">
       <h1>Welcome</h1>
+        <LoginButton />
         <BrowserRouter>
           <Switch>
             <Route path="/app">
---
+++ src/components/MainNav.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MainNav = () => (
+  <div className="navbar-nav ml-auto">
+    Navzona
+  </div>
+);
+
+export default MainNav;
